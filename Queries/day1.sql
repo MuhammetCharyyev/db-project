6091acaa3f88806124c7d356342cd5e63258a4e6
Muhammet <muhammedabuchary@gmail.com>
2022-04-04 14:31:49 -0500

    SQL_Day1

--- Queries/day1.sql
+++ Queries/day1.sql
@@ -31,4 +31,58 @@ from EMPLOYEES;
 select DEPARTMENT_ID, DEPARTMENT_NAME
 from DEPARTMENTS;
 
+--getting unique value from result
+--for ex, only get first name from employee table
+
+select distinct FIRST_NAME--distinct means remove duplicates
+from EMPLOYEES;
+
+--get unique last name
+select distinct LAST_NAME
+from EMPLOYEES;
+
+--getting unique last and first name
+--looking for names with unique full name
+select distinct FIRST_NAME, LAST_NAME
+from EMPLOYEES;
+
+--restricting rows in the result using 'where':
+--operators: =, >, >=, <, <=, !=, <>
+--logic operators: AND, OR
+
+--display employee info for employee first name 'Ellen'
+select * from EMPLOYEES
+where FIRST_NAME = 'Ellen';
+--single quote for Strings, value is case sensitive
+
+--display employee full name info with salary 11000
+select  FIRST_NAME, LAST_NAME, SALARY
+from EMPLOYEES
+where SALARY = 11000;
+
+--display employee with salary 4800 and first name David
+select  FIRST_NAME, LAST_NAME, SALARY
+from EMPLOYEES
+where SALARY = 4800 AND FIRST_NAME = 'David';
+--if you highlight some and push run it will run only highlighted
+
+--display employee with salary 4800 OR first name David
+select  FIRST_NAME, LAST_NAME, SALARY
+from EMPLOYEES
+where SALARY = 4800 OR FIRST_NAME = 'David';
+
+--display employees with salary more 5000 and less 12000
+select FIRST_NAME, LAST_NAME, SALARY
+from EMPLOYEES
+where SALARY > 5000 AND SALARY< 12000;
+
+--display employees that have JOB_ID of:
+--AD_VP
+--AD_ASST
+--FI_ACCOUNT
+--AC_ACCOUNT
+SELECT * FROM EMPLOYEES
+WHERE JOB_ID = 'AD_VP' OR JOB_ID = 'AD_ASST' OR
+      JOB_ID = 'FI_ACCOUNT' OR JOB_ID='AC_ACCOUNT' ;
+
 
