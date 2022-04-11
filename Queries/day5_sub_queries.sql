356b0a00a82e1ef9416e7eabbbfb1e1148552ab6
Muhammet <muhammedabuchary@gmail.com>
2022-04-11 14:07:43 -0500

    SQL_Day5

--- Queries/day5_sub_queries.sql
+++ Queries/day5_sub_queries.sql
@@ -61,5 +61,59 @@ select FIRST_NAME from EMPLOYEES
 where DEPARTMENT_ID = (select DEPARTMENT_ID from DEPARTMENTS
                        where DEPARTMENT_NAME = 'Executive');
 
+                --find first_name of empl
+                --who works in Executive and Admin Depts
+
+-- FIND OUT THE DEPARTMENT_ID OF  Executive
+-- AND Administration Department
+select DEPARTMENT_ID from DEPARTMENTS
+where DEPARTMENT_NAME in ('Executive', 'Administration');
+
+--find first_name of empl
+--who works in dept_id 90 and 10
+select FIRST_NAME from EMPLOYEES
+where DEPARTMENT_ID in (10,90);
+
+--combine results of above both
+select FIRST_NAME from EMPLOYEES
+where DEPARTMENT_ID in (select DEPARTMENT_ID from DEPARTMENTS
+                        where DEPARTMENT_NAME in ('Executive', 'Administration'));
+
+-- result of Subquery can alos be use as table
+--for example select * from  (Subquery goes here)
+select FIRST_NAME, LAST_NAME, FIRST_NAME||' ' ||LAST_NAME as FULL_NAME
+from EMPLOYEES
+-- WHERE FULL_NAME = 'EllenAbel' WE CAN NOT USE ALIAS IN CONDITION
+;
+
+select *
+from (select FIRST_NAME,  FIRST_NAME||' ' ||LAST_NAME as FULL_NAME
+      from EMPLOYEES)
+where FULL_NAME = 'Ellen Abel'; --we can select from created table
+
+--find employee count of that dept with max quantity of empls
+select DEPARTMENT_ID, count(*) as EMPLOYEE_COUNT
+from EMPLOYEES
+group by DEPARTMENT_ID;
+
+select max(EMPLOYEE_COUNT)
+from (select DEPARTMENT_ID, count(*) as EMPLOYEE_COUNT
+      from EMPLOYEES
+      group by DEPARTMENT_ID);
+
+--find dept_id of that dept with max quantity of empls
+select DEPARTMENT_ID
+from (select DEPARTMENT_ID, count(*) as EMPLOYEE_COUNT
+      from EMPLOYEES
+      group by DEPARTMENT_ID);
+
+--dense_rank function that fall into category of analytical functions
+-- dense_rank() over(order by salary)
+select FIRST_NAME, SALARY,
+       dense_rank() over (order by salary desc) as SALARY_RANK
+from EMPLOYEES;
+
+
+
 
 
