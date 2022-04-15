9068bed53424ed307837e7fd17078dc47ece9f41
Muhammet <muhammedabuchary@gmail.com>
2022-04-15 10:41:12 -0500

    JDBC_day3

--- src/test/java/com/cydeo/day3/SpartanSearchTest.java
+++ src/test/java/com/cydeo/day3/SpartanSearchTest.java
@@ -1,11 +1,15 @@
 package com.cydeo.day3;
 
 import com.cydeo.utility.DB_Util;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 
 public class SpartanSearchTest {
+
+
 
     /**
      * Background : on Spartan UI All Data page ,
@@ -21,9 +25,28 @@ public class SpartanSearchTest {
      * search by partial name + gender should
      */
 
+    @BeforeMethod
+    public void setUp() {
+        //create a connection ONLY ONCE for all tests in THIS class
+        String url = "jdbc:oracle:thin:@18.215.145.203:1521:XE";
+        String username = "SP";
+        String password = "SP";
+
+        DB_Util.createConnection(url, username, password);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        //tear down connection ONLY ONCE after all tests in THIS class
+        DB_Util.destroy();
+    }
+
     @Test
     public void testSearchByGender() {
 
+        // Assuming you already used your awesome webdriver knowledge
+        // to get actual result from that total count
+        // or by counting the web table row and got below result
         int actualResultMale = 54;
         int actualResultFemale = 47;
 
@@ -31,11 +54,6 @@ public class SpartanSearchTest {
         // For Male :  SELECT COUNT(*) AS COUNT FROM SPARTANS WHERE GENDER = 'Male'
         // For Female :SELECT COUNT(*) AS COUNT FROM SPARTANS WHERE GENDER = 'Female'
 
-        String url = "jdbc:oracle:thin:@18.215.145.203:1521:XE";
-        String username = "SP";
-        String password = "SP";
-
-        DB_Util.createConnection(url, username, password);
         DB_Util.runQuery("SELECT COUNT(*) AS COUNT FROM SPARTANS WHERE GENDER = 'Male'");
         int expectedMaleResult =  Integer.parseInt( DB_Util.getFirstRowFirstColumn() )  ;
 
